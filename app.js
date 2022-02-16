2a7ed4d20d13b25ec0ec6f4013b65e6aee54fa1c
unknown <PriyaS@WI-CCP-ST-04.wilkescc.edu>
2022-02-16 11:26:58 -0500

    updated

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 // Player name
 var player1 = "Player 1";
 var player2 = "Player 2";
@@ -51,55 +51,4 @@ for(let i=0;i<5;i++)
                             = (player1 + " WINS!");
         }
     }, 2500);
-=======
-// Player name
-var player1 = "Player 1";
-var player2 = "Player 2";
-
-// Function to change the player name
-function editNames() {
-    player1 = prompt("Change Player1 name");
-    player2 = prompt("Change player2 name");
-
-    document.querySelector("p.Player1").innerHTML = player1;
-    document.querySelector("p.Player2").innerHTML = player2;
-}
-
-// Function to roll the dice
-function rollTheDice() {
-    setTimeout(function () {
-        var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-        var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-        var randomNumber3 = Math.floor(Math.random() * 6) + 1;
-        var randomNumber4 = Math.floor(Math.random() * 6) + 1;
-
-        let random=randomNumber1+randomNumber2;
-        let random2=randomNumber3+randomNumber4;
-        
-
-            document.querySelector(".img1").setAttribute("src",
-                "dice" + randomNumber1 + ".png");
-            document.querySelector(".img2").setAttribute("src",
-                "dice" + randomNumber2 + ".png");
-            document.querySelector(".img3").setAttribute("src",
-                "dice" + randomNumber3 + ".png");
-            document.querySelector(".img4").setAttribute("src",
-                "dice" + randomNumber4 + ".png");
-
-        if (random === random2) {
-            document.querySelector("h1").innerHTML = "Draw!";
-        }
-
-        else if (random < random2)
-         {
-            document.querySelector("h1").innerHTML
-                            = (player2 + " WINS!");
-        }
-
-        else {
-            document.querySelector("h1").innerHTML
-                            = (player1 + " WINS!");
-        }
-    }, 2500);
->>>>>>> 009bafb1136765a186a2b29c936b8da73dafea98
 }
